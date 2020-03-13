6ae586b25352b35de12c76acb512bcc26437c644
Michael Jean <mjean@cmmc02yt2lblvch.innova.local>
2020-03-13 17:24:50 -0400

    Documentation update

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="layout__body">
     <div>
       <h1>Janiculum: <br/> A Latin Learning Application</h1>
-      <p>Welcome to Janiculum! This website is primarily a playground for its creator to explore new web technologies. Along the way, it will hopefully provide a variety of tools for the student of Latin.</p>
+      <p>Welcome to Janiculum! This website is primarily a playground for its creator to explore new web technologies. Along the way, it will hopefully provide a variety of tools for the anyone who wants to learn the Latin language.</p>
     </div>
   </div>
 </template>
