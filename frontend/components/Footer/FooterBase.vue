f2a5803ac64382662cf135fdd792adb5df0530d8
Hayes Griffin <hayesguitar@gmail.com>
2020-03-21 15:51:30 -0400

    Deleted 'Text' from Footer

--- frontend/components/Footer/FooterBase.vue
+++ frontend/components/Footer/FooterBase.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="layout__footer">
-    <p class="p--small">Text <i>A Latin Dictionary Founded on Andrews' edition of Freund's Latin dictionary revised, enlarged, and in great part rewritten by Charlton T. Lewis, Ph.D. and Charles Short, LL.D.</i> provided under a CC BY-SA license by Perseus Digital Library, <a href="http://www.perseus.tufts.edu" target="_blank">http://www.perseus.tufts.edu</a>, with funding from The National Endowment for the Humanities.
+    <p class="p--small"><i>A Latin Dictionary Founded on Andrews' edition of Freund's Latin dictionary revised, enlarged, and in great part rewritten by Charlton T. Lewis, Ph.D. and Charles Short, LL.D.</i> provided under a CC BY-SA license by Perseus Digital Library, <a href="http://www.perseus.tufts.edu" target="_blank">http://www.perseus.tufts.edu</a>, with funding from The National Endowment for the Humanities.
 Data accessed from <a href="https://github.com/PerseusDL/lexica/" target="_blank">https://github.com/PerseusDL/lexica/</a> on March, 17, 2020.</p>
   </footer>
 </template>
