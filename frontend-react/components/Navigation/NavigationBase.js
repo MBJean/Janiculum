027be7b881148e8e907b678a014795ca9ba5c38e
Michael Jean <michaelbrianjean@gmail.com>
2021-01-02 11:05:25 -0500

    Get started with some of the client GraphQL

--- frontend-react/components/Navigation/NavigationBase.js
+++ frontend-react/components/Navigation/NavigationBase.js
@@ -1,7 +1,19 @@
-const NavigationBase = () => (
-  <div>
-    NAVBAR
-  </div>
-);
+import Link from 'next/link'
+import styles from './NavigationBase.module.scss'
 
-export default NavigationBase;
+export default function NavigationBase() {
+
+  return (
+    <nav className={styles.nav} role="navigation" aria-label="main navigation">
+      <Link href="/">
+        <a>
+          <img
+            src="/images/logo.png"
+            alt="The Janiculum Logo, depicting the two-headed Roman god Janus"
+            className={styles.logo}
+          />
+        </a>
+      </Link>
+    </nav>
+  );
+}
--- frontend-react/graphql/client.js
+++ frontend-react/graphql/client.js
@@ -1,6 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:8000/graphql',
+  credentials: 'same-origin'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('janiculum-token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 export default new ApolloClient({
-  uri: process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
